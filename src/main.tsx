68f0b28e6b746de8b46b26cd38c401c7644cc836
IgnacioCastilloFranco <github_portfolio@outlook.es>
2025-12-13 11:39:11 +0100

    doc

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-//This code is the entry point that bootstraps your React application and mounts it to the DOM (Document Object Model). 
+//This code is the entry point that bootstraps your React application and mounts it to the DOM (Document Object Model). mounts a React component tree into the browser's actual DOM. This is the bridge between React's virtual DOM and the real DOM that users see in the browser
 ReactDOM.createRoot(document.getElementById('root')!).render(//retrieves the HTML element with the id root from your index.html file, which is an empty <div id="root"></div> that serves as the container for your entire React app
-  <React.StrictMode>
+  <React.StrictMode>                                        
     <App />
   </React.StrictMode>,
 )
